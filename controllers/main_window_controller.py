f9cda1d756d54b9b2d42317b39757dcbae62b7aa
Drvoo <39501584+Drvoo@users.noreply.github.com>
2020-12-21 16:00:34 +0100

    improved ui design / setting radio buttons randomly

--- controllers/main_window_controller.py
+++ controllers/main_window_controller.py
@@ -1,28 +1,38 @@
 from models.main_model import MainModel
+import view.main_view
 import time
 import threading
 
-
 class MainWindowController:
-    def __init__(self, view):
+    def __init__(self, view: view.main_view.Ui_MainWindow):
         self.view = view
         self.model = MainModel(self)
         self.working = True
-        worker_thread = threading.Thread(target=MainWindowController.__worker_thread, args=[self])
-        worker_thread.start()
-
-
+        self.dict_rb_buttons = {
+            "large_garage": self.view.rb_large_garage,
+            "celler_door": self.view.rb_celler_door,
+            "attic_door": self.view.rb_attic_door,
+            "garden_door": self.view.rb_garden_door,
+            "garden_door_a": self.view.rb_garden_door_a,
+            "garden_door_b": self.view.rb_garden_door_b,
+            "light_garage": self.view.rb_light_garage,
+            "lights_attic": self.view.rb_lights_attic,
+            "motion_detector_1": self.view.rb_motion_detector_1,
+            "motion_detector_2": self.view.rb_motion_detector_2,
+            "motion_detector_3": self.view.rb_motion_detector_3,
+            "small_garage": self.view.rb_small_garage
+        }
+        self.model.start_work()
 
     def connect_signals(self):
         pass
 
-    def __worker_thread(self) -> None:
-        while False:
-            time.sleep(2)
-            self.view.rb_attic_door.toggle()
-            self.view.rb_celler_door.toggle()
-            self.view.rb_large_garage.toggle()
+    # view to model
 
+    # model to view
+    def update_signals(self, status_rb_buttons: dict):
+        for name, status in status_rb_buttons.items():
+            time.sleep(0.05)
+            self.dict_rb_buttons[name].setChecked(status)
+        pass
 
-    def __stop_worker_thread(self):
-        self.working = False
--- models/main_model.py
+++ models/main_model.py
@@ -1,3 +1,41 @@
+import threading
+import time
+import random
+
 class MainModel:
     def __init__(self, controller):
         self.controller = controller
+        self.worker_thread = None
+        self.working = False
+        self.dict_rb_buttons = {
+            "large_garage": False,
+            "celler_door": False,
+            "attic_door": False,
+            "garden_door": False,
+            "garden_door_a": False,
+            "garden_door_b": False,
+            "light_garage": False,
+            "lights_attic": False,
+            "motion_detector_1": False,
+            "motion_detector_2": False,
+            "motion_detector_3": False,
+            "small_garage": False
+        }
+
+    def start_work(self):
+        self.working = True
+        self.worker_thread = threading.Thread(target=self.work)
+        self.worker_thread.start()
+
+    def work(self):
+        while self.working:
+            for name, status in self.dict_rb_buttons.items():
+                # TODO: Check signals of raspbarrypi
+                self.dict_rb_buttons[name] = bool(random.randrange(0, 2))
+            self.controller.update_signals(self.dict_rb_buttons)
+            time.sleep(2)
+        pass
+
+    def stop_work(self):
+        self.working = False
+        pass
